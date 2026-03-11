213ee8a46acf6d89389504968b5fa52f95533b5a
danielmirandalog-debug <danielmiranda.log@gmail.com>
2026-03-10 23:51:40 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,6 @@
-const CACHE_NAME = "xibungao-taxas-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./app.js",
-  "./style.css"
-];
-
-self.addEventListener("install", event => {
-
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-
+self.addEventListener('install', function(e) {
+console.log('Service Worker instalado');
 });
 
-self.addEventListener("fetch", event => {
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-
+self.addEventListener('fetch', function(event) {
 });
